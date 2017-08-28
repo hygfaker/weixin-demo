772d6c57c271b28540ce02b72a17e9b787e86371
yans67 <1018743361@qq.com>
2017-08-28 18:42:00 +0800

    添加 wxpublic dao

---
+++ src/main/java/com/demo/wechat/dao/WXPublic.java
@@ -0,0 +1,107 @@
+package com.demo.wechat.dao;
+
+/**
+ * Created by huangyg on 2017/8/28.
+ */
+public class WXPublic {
+    private String wxPublicCode;
+    private String wxPublicOpenid;
+    private String wxPublicName;
+    private String wxPublicNickName;
+    private String wxPublicHeadImg;
+    private String wxPublicQrcode;
+    private String wxPublicAppid;
+    private String wxPublicAppSerct;
+    private String wxPublicToken;
+    private String wxPublicAeskey;
+    private String wxPublicUrl;
+
+    public String getWxPublicCode() {
+        return wxPublicCode;
+    }
+
+    public void setWxPublicCode(String wxPublicCode) {
+        this.wxPublicCode = wxPublicCode;
+    }
+
+    public String getWxPublicOpenid() {
+        return wxPublicOpenid;
+    }
+
+    public void setWxPublicOpenid(String wxPublicOpenid) {
+        this.wxPublicOpenid = wxPublicOpenid;
+    }
+
+    public String getWxPublicName() {
+        return wxPublicName;
+    }
+
+    public void setWxPublicName(String wxPublicName) {
+        this.wxPublicName = wxPublicName;
+    }
+
+    public String getWxPublicNickName() {
+        return wxPublicNickName;
+    }
+
+    public void setWxPublicNickName(String wxPublicNickName) {
+        this.wxPublicNickName = wxPublicNickName;
+    }
+
+    public String getWxPublicHeadImg() {
+        return wxPublicHeadImg;
+    }
+
+    public void setWxPublicHeadImg(String wxPublicHeadImg) {
+        this.wxPublicHeadImg = wxPublicHeadImg;
+    }
+
+    public String getWxPublicQrcode() {
+        return wxPublicQrcode;
+    }
+
+    public void setWxPublicQrcode(String wxPublicQrcode) {
+        this.wxPublicQrcode = wxPublicQrcode;
+    }
+
+    public String getWxPublicAppid() {
+        return wxPublicAppid;
+    }
+
+    public void setWxPublicAppid(String wxPublicAppid) {
+        this.wxPublicAppid = wxPublicAppid;
+    }
+
+    public String getWxPublicAppSerct() {
+        return wxPublicAppSerct;
+    }
+
+    public void setWxPublicAppSerct(String wxPublicAppSerct) {
+        this.wxPublicAppSerct = wxPublicAppSerct;
+    }
+
+    public String getWxPublicToken() {
+        return wxPublicToken;
+    }
+
+    public void setWxPublicToken(String wxPublicToken) {
+        this.wxPublicToken = wxPublicToken;
+    }
+
+    public String getWxPublicAeskey() {
+        return wxPublicAeskey;
+    }
+
+    public void setWxPublicAeskey(String wxPublicAeskey) {
+        this.wxPublicAeskey = wxPublicAeskey;
+    }
+
+    public String getWxPublicUrl() {
+        return wxPublicUrl;
+    }
+
+    public void setWxPublicUrl(String wxPublicUrl) {
+        this.wxPublicUrl = wxPublicUrl;
+    }
+
+}
