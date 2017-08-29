a9b945eeebf0d141ad2856ed51de86af2006a95c
yans67 <1018743361@qq.com>
2017-08-29 16:04:30 +0800

    修改成 mybatis 的注解版

--- src/main/java/com/demo/wechat/WxMpDemoApplication.java
+++ src/main/java/com/demo/wechat/WxMpDemoApplication.java
@@ -1,5 +1,6 @@
 package com.demo.wechat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
@@ -11,10 +12,9 @@ import javax.servlet.MultipartConfigElement;
  * @author Binary Wang(https://github.com/binarywang)
  */
 @SpringBootApplication
+@MapperScan("com.demo.wechat.mapper")
 public class WxMpDemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WxMpDemoApplication.class, args);
     }
-
 }
--- src/test/java/com/demo/wechat/dao/WXPublicTest.java
+++ src/test/java/com/demo/wechat/dao/WXPublicTest.java
@@ -1,21 +1,52 @@
 package com.demo.wechat.dao;
 
 
+import com.demo.wechat.mapper.WXPublicMapper;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.InputStream;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class WXPublicTest {
-    private static final String namespace="com.demo.wechat.dao.WXPublic";
+
+    @Autowired
+    private WXPublicMapper wxPublicMapper;
+
+    @org.junit.Test
+    public void insertTest()throws Exception{
+        //增加
+        WXPublic wxPublic=new WXPublic();
+        wxPublic.setWxPublicAeskey("aeskey2222");
+        wxPublic.setWxPublicAppid("appid222");
+        wxPublic.setWxPublicAppSerct("appseret222");
+        wxPublic.setWxPublicHeadImg("img");
+        wxPublic.setWxPublicName("name");
+        wxPublic.setWxPublicNickName("nickname");
+        wxPublic.setWxPublicOpenid("openid");
+        wxPublic.setWxPublicQrcode("qrcode");
+        wxPublic.setWxPublicToken("token");
+        wxPublic.setWxPublicUrl("url");
+        int insertCount = wxPublicMapper.insert(wxPublic);
+        assert insertCount==1;
+    }
 
     @org.junit.Test
     public void test() throws Exception {
         InputStream configStream = Resources.getResourceAsStream("mybatis-wxconfig.xml");
+
         SqlSessionFactory sqlSessionFactory= new SqlSessionFactoryBuilder().build(configStream);
-        SqlSession  sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+
         try{
             WXPublicMapper wxPublicMapper = sqlSession.getMapper(WXPublicMapper.class);
 
